0527e5cce15084d30ebe52d9b74d09176daa6282
aryush <aryush.garg@gmail.com>
2018-01-05 20:53:18 +0530

    check for no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
   console.log(props.ingredients.meat);
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(ingKey => {
       console.log(props.ingredients[ingKey]);
       return [...Array(props.ingredients[ingKey])].map((_, i) => {
         return <BurgerIngredient key={ingKey+i} type={ingKey} />
-      })
-    });
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+  console.log(transformedIngredients);
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients.</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
